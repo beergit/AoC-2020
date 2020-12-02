999250ae47c999c91c1bc211443b4870bf170271
igittigitt <beergit@go4more.de>
2020-12-02 07:38:44 +0100

    final solution day #2

---
+++ advent2/advent2.rb
@@ -0,0 +1,30 @@
+class Advent2
+  def initialize(input_file)
+    @policities = File.new(input_file).readlines.map{|line| Policity.new(line)}
+  end
+
+  def part1
+    @policities.count{|pol| pol.valid_part1?}
+  end  
+
+  def part2
+    @policities.count{|pol| pol.valid_part2?}
+  end 
+
+private
+  class Policity
+    def initialize(line)
+      pm = /(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)/.match(line)
+      @pol_range, @pol_char, @pol_passwd = (pm[1].to_i..pm[2].to_i), pm[3], pm[4]
+    end
+
+    def valid_part1?
+      @pol_range.include? @pol_passwd.count(@pol_char)
+    end
+
+    def valid_part2?
+      (@pol_passwd[@pol_range.first - 1] == @pol_char) ^ (@pol_passwd[@pol_range.last - 1] == @pol_char)
+    end
+  end
+end
+
---
+++ advent2/tc_advent2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+require_relative "advent2"
+require "test/unit"
+class TestAdvent2 < Test::Unit::TestCase
+ 
+  def setup
+    @test1_1  = ['test_input.txt', 2]
+    @test2_1  = ['test_input.txt', 1]
+  end  
+
+  def test_part1
+    assert_equal(@test1_1[1], Advent2.new(@test1_1[0]).part1)
+  end
+
+  def test_part2
+    assert_equal(@test2_1[1], Advent2.new(@test2_1[0]).part2)
+  end
+end
