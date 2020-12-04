f0d2b02aa9f12d31bb0074600f8ee59fbd89f0bc
igittigitt <beergit@go4more.de>
2020-12-04 09:34:52 +0100

    final solution day#4

---
+++ advent4/advent4.rb
@@ -0,0 +1,38 @@
+class Advent4
+  def initialize(input_file)
+    @passports = File.read(input_file).split(/\n{2,}/).map{|id| Passport.new(id)}
+  end
+
+  def part1
+    @passports.count{|p| p.valid_part1?}
+  end
+  def part2
+    @passports.count{|p| p.valid_part2?}
+  end
+
+  private
+    class Passport
+      HeightRanges = {:cm => (150..193), :in => (59..76)}
+      EyeColors = %w(amb blu brn gry grn hzl oth)
+      Validations = {
+        :byr => lambda {|v| (1920..2002).include? v.to_i},
+        :iyr => lambda {|v| (2010..2020).include? v.to_i},
+        :eyr => lambda {|v| (2020..2030).include? v.to_i},
+        :hgt => lambda {|v| v =~ /^(\d*)((cm)|(in))$/ and HeightRanges[$2.to_sym].include?($1.to_i)},
+        :hcl => lambda {|v| v =~ /^#[0-9a-f]{6}$/},
+        :ecl => lambda {|v| EyeColors.include? v},
+        :pid => lambda {|v| v =~ /^\d{9}$/}
+      }
+      def initialize(id)
+        @ids = id.split.map{|id| id.split(':')}.map{|id| [id[0].to_sym, id[1]]}.to_h
+      end
+
+      def valid_part1?
+        (Validations.keys - @ids.keys).empty?
+      end
+
+      def valid_part2?
+        Validations.all?{|k, v| @ids[k] and v[@ids[k]]}
+      end  
+    end
+end
---
+++ advent4/tc_advent4.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+require_relative "advent4"
+require "test/unit"
+class TestAdvent4 < Test::Unit::TestCase
+ 
+  def setup
+    @test1_1  = ['test_part1_input.txt', 2]
+    @test2_1  = ['test_part2_valid_input.txt', 4]
+    @test2_2  = ['test_part2_invalid_input.txt', 0]
+  end  
+
+  def test_part1
+    assert_equal(@test1_1[1], Advent4.new(@test1_1[0]).part1)
+  end
+
+  def test_part2
+    assert_equal(@test2_1[1], Advent4.new(@test2_1[0]).part2)
+    assert_equal(@test2_2[1], Advent4.new(@test2_2[0]).part2)
+  end
+end
