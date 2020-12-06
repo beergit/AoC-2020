1dd46121bfe820f1ee577fa3ca6809564856b524
igittigitt <beergit@go4more.de>
2020-12-06 07:27:38 +0100

    solution day #6

---
+++ advent6/advent6.rb
@@ -0,0 +1,14 @@
+class Advent6
+  def initialize(input_file)
+    @answers = File.read(input_file).split(/\n{2,}/)
+  end
+
+  def part1
+    @answers.reduce(0){|s, a| s += a.split.map(&:chars).reduce(:+).uniq.size}
+  end
+
+  def part2
+    @answers.reduce(0){|s, a| s += a.split.map(&:chars).reduce(:&).size}
+  end    
+end
+
