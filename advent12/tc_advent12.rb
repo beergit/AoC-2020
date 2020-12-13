91a4984968677e2e2a2ae943a2b5f0123cd89f81
igittigitt <beergit@go4more.de>
2020-12-13 12:10:34 +0100

    undo commit day12

--- advent12/tc_advent12.rb
+++ advent12/tc_advent12.rb
@@ -1,23 +1,11 @@
 #!/usr/bin/ruby
-require_relative 'advent12'
 require 'test/unit'
 class TestAdvent12 < Test::Unit::TestCase
-
-TestData = <<EOF
-F10
-N3
-F7
-R90
-F11
-EOF
-
-
+ 
   def setup
-    @test1_1  = [TestData, 25]
   end  
 
   def test_part1
-    assert_equal(@test1_1[1], Advent12.new(@test1_1[0]).part1)
   end
 
   def test_part2
