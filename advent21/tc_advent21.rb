e707c6c195e5fb08dbb77436aa4314ebb7a364be
igittigitt <beergit@go4more.de>
2020-12-22 13:35:04 +0100

    init day21

--- advent21/tc_advent21.rb
+++ advent21/tc_advent21.rb
@@ -1,11 +1,21 @@
 #!/usr/bin/ruby
+require_relative 'advent21'
 require 'test/unit'
 class TestAdvent21 < Test::Unit::TestCase
+
+TestData1_1 = <<EOF
+  mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+  trh fvjkl sbzzf mxmxvkd (contains dairy)
+  sqjhc fvjkl (contains soy)
+  sqjhc mxmxvkd sbzzf (contains fish)
+EOF
  
   def setup
-  end  
+    @test1_1  = [TestData1_1, 5]
+  end
 
   def test_part1
+    assert_equal(@test1_1[1], Advent21.new(@test1_1[0]).part1)
   end
 
   def test_part2
